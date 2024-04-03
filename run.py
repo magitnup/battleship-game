6bac40f40b482fa321ddac1c27332f612329d6f6
magitnup <lpunti@ope.at>
2024-04-03 05:07:42 +0200

    initial commit shows boards layout

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Plan:
+# 2 boards: one computer board and one player board
+# computer board randomly selects places for ships
+# player board allows for players choice to place ships
+# validation for locking in the "hits" on the ships
+# no repeat guesses, no out of bounds guesses
+# smart guesses on the computers side after hitting a ship
+# boards are 10x10 -> input 33 = row 3 column 3 starting with 0 - 9 for each
+print("     BATTLESHIPS:    ")
+print("   0  1  2  3  4  5  6  7  8  9")
+for x in range(10):
+    print(x," _ "*10)

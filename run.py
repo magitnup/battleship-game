76b59d1c59a1f4291d8794be269f5459d81ca27b
magitnup <lpunti@ope.at>
2024-04-03 05:45:08 +0200

    shows wether a ship got ,missed,  hit or sunk

--- run.py
+++ run.py
@@ -6,7 +6,31 @@
 # no repeat guesses, no out of bounds guesses
 # smart guesses on the computers side after hitting a ship
 # boards are 10x10 -> input 33 = row 3 column 3 starting with 0 - 9 for each
-print("     BATTLESHIPS:    ")
-print("   0  1  2  3  4  5  6  7  8  9")
-for x in range(10):
-    print(x," _ "*10)
+def show_board(good,blunder,great):
+    print("            battleships    ")
+    print("     0  1  2  3  4  5  6  7  8  9")
+ 
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _ "
+            if place in blunder:
+                ch = " x "
+            elif place in good:
+                ch = " o "
+            elif place in great:
+                ch = " s "  
+            row = row + ch
+            place = place + 1
+             
+        print(x," ",row)
+        
+blunder = [0,1,2,3,4,5,6,7,8,9]
+
+good = [10,11,12,13,14,15,16,17,18,19]
+
+great = [20,21,22,23,24,25,26,27,28,29]
+
+
+show_board(blunder, good, great)
